9056840c0c5adc4c6adb35decd398aa89b9c24b7
erik <zerpaerik@gmail.com>
2019-04-04 19:18:57 -0430

    ticket ventas

--- resources/views/existencias/ventas/ticket.blade.php
+++ resources/views/existencias/ventas/ticket.blade.php
@@ -64,27 +64,27 @@ margin-top: -20px;
 <body>
 
 
-   <div class="" style="font-size: 20px; text-align: center;">
+   <div class="" style="font-size: 60px; text-align: center;">
 		<p><strong>GRUPO MASTER SALUD</strong></p>
 	    <p><strong>TICKET:0000{{$ticket->id}}</strong></p>
 	</div>
 
-    <div class="" style="font-size: 20px;">
+    <div class="" style="font-size: 60px;">
 		<p><strong>FECHA:{{ $ticket->created_at}}</strong></p>
 	</div>
 
-	<div class="" style="font-size: 20px;">
+	<div class="" style="font-size: 60px;">
 		<p><strong>PACIENTE:{{ $ticket->nombres}},{{ $ticket->apellidos}}</strong></p>
 	</div>
 
 	@foreach($productos as $p)
-	<div class="" style="font-size: 20px;">
+	<div class="" style="font-size: 60px;">
 		<p><strong>PRODUCTO:{{ $p->nombre}}- CANTIDAD {{ $p->cantidad}}
 		</strong></p>
 	</div>
 	@endforeach
 
-	<div class="" style="font-size: 20px;">
+	<div class="" style="font-size: 60px;">
 		<p><strong>MONTO TOTAL:{{ $monto->monto}}</strong></p>
 	</div>
 
